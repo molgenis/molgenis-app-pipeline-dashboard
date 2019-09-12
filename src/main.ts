083c476fc5fb39221981abf4e86d1342c748297c
jprofijt <jouke.profijt@gmail.com>
2019-09-12 15:42:33 +0200

    fix(main.ts): fixed bootstrap-vue.css import

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,7 @@ Vue.config.productionTip = false
 Vue.use(BootstrapVue)
 
 import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 new Vue({
   render: h => h(App)
