6934c7f0bdc547618169c3719478266a67794976
jprofijt <jouke.profijt@gmail.com>
2019-09-12 15:41:41 +0200

    feat(progressbar.vue): added progressbar component
    Progress bar for displaying completed steps

---
+++ src/components/Track&Trace-Components/ProgressBar.vue
@@ -0,0 +1,23 @@
+<template>
+    <b-progress :variant="!finished ? 'primary' : 'success'" :max="totalSteps" show-progress animated>
+          <b-progress-bar :value="step">
+            <strong>{{ step }} / {{ totalSteps }}</strong>
+          </b-progress-bar>
+        </b-progress>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+    name: 'progress-bar',
+    props: {
+        step: Number,
+        totalSteps: Number,
+        finished: Boolean
+    }
+})
+</script>
+
+<style>
+
+</style>
