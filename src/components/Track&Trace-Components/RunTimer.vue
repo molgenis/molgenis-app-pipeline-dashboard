abf0893162cb0cb28536eee2fdba66393d172cd7
jprofijt <jouke.profijt@gmail.com>
2019-09-17 09:55:11 +0200

    feat(added timer for projects): added timer for projects
    Calculates the time it has been since start of jobs

---
+++ src/components/Track&Trace-Components/RunTimer.vue
@@ -0,0 +1,40 @@
+<template>
+    <p v-if="started">{{hours}}:{{minutes}}:{{seconds}}</p>
+    <p v-else>Not Started</p>
+</template>
+
+<script>
+import Vue from 'vue'
+export default Vue.extend({
+    name: 'run-timer',
+    props: {
+        startTime: Number,
+        finishTime: Number,
+        started: Boolean
+    },
+    computed: {
+        time: function () {
+            return this.finishTime - this.startTime
+        },
+        seconds: function () {
+            return Math.round(((this.time/1000) % 3600) % 60)
+        },
+        minutes: function() {
+            return Math.round(((this.time/1000) / 3600) % 60)
+        },
+        hours: function () {
+            return Math.round((this.time/1000) / 3600)
+        }
+        
+    },
+    methods: {
+    },
+    mounted () {
+    }
+    
+})
+</script>
+
+<style scoped>
+
+</style>
