343837978366d1ac1912bdac0ba2900f3b64bac6
jprofijt <jouke.profijt@gmail.com>
2019-10-02 15:56:45 +0200

    test(progress bar tests): wrote small tests for progress bar component
    Added the first tests which are for the progress bar component

---
+++ tests/unit/components/TrackAndTrace/ProgressBarComponent.spec.ts
@@ -0,0 +1,30 @@
+import { createLocalVue, mount } from '@vue/test-utils'
+import BootstrapVue from 'bootstrap-vue'
+import ProgressBar from '@/components/Track&Trace-Components/ProgressBar.vue'
+
+const localVue = createLocalVue()
+
+localVue.use(BootstrapVue)
+
+
+describe('ProgressBar.vue', () => {
+  it('renders a bootstrap progress bar when passed', () => {
+    const step = 44
+    const totalSteps = 120
+    const wrapper = mount(ProgressBar, {
+      localVue,
+      propsData: { step, totalSteps }
+    })
+    expect(wrapper.text()).toMatch(step + ' / ' + totalSteps)
+  })
+
+  it('Emits finished when progressbar is complete', () => {
+    const step = 2
+    const totalSteps = 2
+    const wrapper = mount(ProgressBar, {
+      localVue,
+      propsData: {step, totalSteps}
+    })
+    expect(wrapper.emitted('progress-finish')).toBeTruthy()
+  })
+})
