5f02f45a1734035b35207b29e4b7c79fd9e019dc
jprofijt <jouke.profijt@gmail.com>
2019-10-04 11:00:21 +0200

    feat(runtimestatistics): implemented outlier calculation using SD
    Runtime outliers get calculated using standard deviation

--- src/components/RunTimeStatistics.vue
+++ src/components/RunTimeStatistics.vue
@@ -5,10 +5,15 @@
           <apexchart type="line" :options="chartOptions" :series="series"></apexchart>
         </b-container>
       </b-col>
-      <b-col cols="4" class="p-2 h-100">
-        <b-container class="border border-primary p-0" fluid></b-container>
+      <b-col cols="2" class="p-2 h-100">
+        <b-container class="border border-primary p-0" fluid>
+          <b-list-group>
+            <b-list-group-item>Outliers</b-list-group-item>
+            <b-list-group-item v-for="outlier in outliers" :key="outlier">Run: {{outlier[1]}}, {{outlier[0]}}</b-list-group-item>
+          </b-list-group>
+        </b-container>
       </b-col>
-       <b-col cols="4" class="p-2 h-100">
+       <b-col cols="6" class="p-2 h-100">
          <b-container class="border border-primary p-0" fluid></b-container>
        </b-col>
     </b-row>  
@@ -26,7 +31,8 @@ export default {
       max: 20,
       average: 8,
       xAnnotations: [],
-      threshold: 20
+      threshold: 12,
+      outliers: []
       }
   },
   computed: {
@@ -47,7 +53,23 @@ export default {
                   color: '#fff',
                   background: '#775DD0'
                 },
-                text: 'Average runtime'
+                text: 'Average runtime: 0'.replace('0', (Math.round(this.average * 10)/10))
+              }
+            },
+            {
+              y: this.average + this.threshold,
+              strokeDashArray: 10,
+              
+              borderColor: '#a57f01',
+              label: {
+                borderColor: '#a57f01',
+                position: 'right',
+                offsetY: -20,
+                style: {
+                  color: '#fff',
+                  background: '#a57f01'
+                },
+                text: 'Threshold: 0'.replace('0', (Math.round((this.average + this.threshold) * 10) / 10))
               }
             }
           ]}
@@ -100,17 +122,20 @@ export default {
     series: function () {
       const numArray = Array.from(this.runTimes, x => Object.values(x)[0])
       this.xAnnotations = []
+      
+      this.average = this.findAverageOfNormalValues(numArray)
+      this.threshold = this.getSD(numArray, this.average)
+
       for (let i = 0; i < this.runTimes.length; i++) {
         let obj = this.runTimes[i]
         let key = Object.keys(obj)[0]
         let value = Object.values(obj)[0]
-        if (value >= this.threshold) {
+        if (value >= this.average + this.threshold) {
           this.xAnnotations.push(this.CreateXannotation(i + 1, key))
+          this.outliers.push([key, i + 1])
         }
       }
-        
 
-      this.average = this.findAverage(numArray)
       const max = Math.max(...numArray)
       if (max > 20) {
         this.max = max + 10
@@ -124,54 +149,65 @@ export default {
     }
   },
   methods: {
+    getSD(numArray, average) {
+      let sumOfDistance = 0
+      numArray.forEach((x) => {
+        sumOfDistance += Math.pow(x - average, 2)
+      })
+
+      return Math.sqrt(sumOfDistance/numArray.length)
+    },
     findAverage(numArray) {
       let sum = 0
       numArray.forEach((x) => {
         sum += x
       })
+      
+      const avg = sum / numArray.length
 
-      const avg = sum / 10
-      console.log(avg)
       return avg
     },
-    test() {
-      this.chartOptions.annotations.xaxis.push(
-        {
-              x: 5.9,
-              x2: 6.1,
-              borderColor: '#a57f01',
-              fillColor: '#e3ae00',
-              opacity: 0.5,
-              label: {
-                borderColor: '#e3ae00',
-                style: {
-                  fontSize: '10px',
-                  color: '#fff',
-                  background: '#e3ae00',
-                },
-                text: '190814_NB501043_...'
-                }
-              }
-      )
+    findAverageOfNormalValues(numArray) {
+      const average = this.findAverage(numArray)
+      const SD = this.getSD(numArray, average)
+      const cutOff = SD
+      const lower = average - cutOff
+      const higher = average + cutOff
+      const filteredArray = numArray.filter((x) => {return x > lower && x < higher})
+
+      return this.findAverage(filteredArray)
     },
     CreateXannotation(coordinate, name) {
+      let labelPosition = 'top'
+      let position1 = coordinate - 0.1
+      let position2 = coordinate + 0.1
+      if (coordinate == 1) {
+        position1 += 0.1
+      } else if (coordinate == 10){
+        position2 -= 0.1
+
+      }
       return {
-              x: coordinate - 0.1,
-              x2: coordinate + 0.1,
-              borderColor: '#a57f01',
-              fillColor: '#e3ae00',
-              opacity: 0.5,
-              label: {
-                borderColor: '#e3ae00',
-                style: {
-                  fontSize: '10px',
-                  color: '#fff',
-                  background: '#e3ae00',
-                },
-                text: name
-                }
-              }
-    }
+        x: position1,
+        x2: position2,
+        borderColor: '#a57f01',
+        fillColor: '#e3ae00',
+        opacity: 0.7,
+        label: {
+          borderColor: '#e3ae00',
+          orientation: 'vertical',
+          position: labelPosition,
+          style: {
+            fontSize: '5mm',
+            color: '#fff',
+            background: '#e3ae00',
+          },
+          offsetX: 45,
+          offsetY: 10,
+          text: name
+          }
+        } 
+      }
   }
 }
 </script>
