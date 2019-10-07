567ac29fe99e24865979f0e536a51ff771e6354b
jprofijt <jouke.profijt@gmail.com>
2019-10-07 14:01:03 +0200

    feat(steptracker): added warning icon and color
    when a step has a warning, correct icon and color will be shown

--- src/components/Track&Trace-Components/RunTableStepTracker.vue
+++ src/components/Track&Trace-Components/RunTableStepTracker.vue
@@ -1,110 +1,122 @@
 <template>
-        <b-row class="progressbar" >
-            <b-col v-for="step in steps" :key="step" :class="getStepStatus(step)" class="circle-container ">
-                <div class="container">
-                    <span class=""> 
-                        {{step}}
-                    </span>
-                    <span>
-                        <font-awesome-icon v-if="isFinalStep(step) && StepRunning(step) && !error" icon="check-circle"/>
-                        <b-spinner v-else-if="StepRunning(step) && !error" variant="primary" small/> 
-                        <font-awesome-icon v-else-if="StepRunning(step) && error" icon="exclamation-circle"/> 
-                        <font-awesome-icon v-else-if="StepComplete(step) && !error" icon="check-circle"/>
-                    </span>
-                </div>
-                
-            </b-col>
-        </b-row>
+  <b-row class="progressbar" >
+    <b-col v-for="step in steps" :key="step" :class="getStepStatus(step)" class="circle-container ">
+      <div class="container">
+        <span class="">
+          {{step}}
+        </span>
+        <span>
+          <template v-if="StepRunning(step)">
+            <font-awesome-icon v-if="error" icon="exclamation-circle"/>
+            <font-awesome-icon v-else-if="warning" icon="exclamation-triangle"/>
+            <font-awesome-icon v-else-if="isFinalStep(step)" icon="check-circle"/>
+            <b-spinner v-else variant="primary" small/>
+          </template>
+          <font-awesome-icon v-else-if="StepComplete(step) && !error" icon="check-circle"/>
+        </span>
+      </div>
+    </b-col>
+  </b-row>
 </template>
 
 <script>
 export default {
-    name: 'step-tracker',
-    props: {
-        steps: {
-            type: Array,
-            required: false,
-            default: ['demultiplexing', 'rawcopy','running', 'resultcopy', 'finished']
-        },
-
-        currentStep: {
-            type: Number,
-            required: false,
-            default: 0
-        },
-
-        error: {
-            type: Boolean,
-            required: false,
-            default: false
-        },
-
-        started: {
-            type: Boolean,
-            required: false,
-            default: true
-        }
+  name: 'step-tracker',
+  props: {
+    steps: {
+      type: Array,
+      required: false,
+      default: function () {
+        return ['demultiplexing', 'rawcopy', 'running', 'resultcopy', 'finished']
+      }
     },
-    methods: {
-        /**
-         * Finds status of current step
-         * @returns String status
-         */
-        getStepStatus(step) {
-            if (this.isFinalStep(step) && this.StepRunning(step)) {
-                return 'complete'
-            } else if (this.steps[this.currentStep] === step) {
-                if (!this.started) {
-                    return ''
-                }
-                if (!this.error) { 
-                    return 'running'
-                }
-                    return 'error'
-            } else {
-                if (this.StepComplete(step)) {
-                    if (this.error) {
-                        return 'error'
-                    } else {
-                        return 'complete'
-                    }
-                } else {
-                    return ''
-                }
-            }
-        },
-        /**
-         * Checks if step is currently running
-         * @param step String 
-         * @returns Boolean
-         */
-        StepRunning(step) {
-            if (!this.started) {
-                return false
-            }
-            return this.steps[this.currentStep] === step
-        },
-        /**
-         * Checks if step has been completed
-         * @returns Boolean
-         */
-        StepComplete(step) {
-            let index = this.steps.indexOf(step)
-            if (!this.started) {
-                return false
-            }
-            
-            return ((index < this.currentStep))
-        },
-        /**
-         * Checks if step is final step
-         * @returns Boolean
-         */
-        isFinalStep(step) {
-            let index = this.steps.indexOf(step)
-            return (step === this.steps.slice(-1)[0])
-        }
+
+    currentStep: {
+      type: Number,
+      required: false,
+      default: 0
+    },
+
+    error: {
+      type: Boolean,
+      required: false,
+      default: false
+    },
+
+    warning: {
+      type: Boolean,
+      required: false,
+      default: false
+    },
+
+    started: {
+      type: Boolean,
+      required: false,
+      default: true
     }
+  },
+  methods: {
+    /**
+     * Finds status of current step
+     * @returns String status
+     */
+    getStepStatus (step) {
+      if (this.isFinalStep(step) && this.StepRunning(step)) {
+        return 'complete'
+      } else if (this.steps[this.currentStep] === step) {
+        if (!this.started) {
+          return ''
+        }
+        if (this.error) {
+          return 'error'
+        }
+        if (this.warning) {
+          return 'warning'
+        }
+        return 'running'
+      } else {
+        if (this.StepComplete(step)) {
+          if (this.error) {
+            return 'error'
+          } else {
+            return 'complete'
+          }
+        } else {
+          return ''
+        }
+      }
+    },
+    /**
+     * Checks if step is currently running
+     * @param step String
+     * @returns Boolean
+     */
+    StepRunning (step) {
+      if (!this.started) {
+        return false
+      }
+      return this.steps[this.currentStep] === step
+    },
+    /**
+     * Checks if step has been completed
+     * @returns Boolean
+     */
+    StepComplete (step) {
+      let index = this.steps.indexOf(step)
+      if (!this.started) {
+        return false
+      }
+      return ((index < this.currentStep))
+    },
+    /**
+     * Checks if step is final step
+     * @returns Boolean
+     */
+    isFinalStep (step) {
+      let index = this.steps.indexOf(step)
+      return (step === this.steps.slice(-1)[0])
+    }
+  }
 }
 </script>
 
@@ -114,68 +126,76 @@ export default {
 @import '../../../node_modules/bootstrap-vue/src/index.scss';
 
 .progressbar {
-    counter-reset: step;
+  counter-reset: step;
 
 }
 
 .circle-container {
-    list-style-type: none;
-    float: left;
-    font-size: 14px;
-    text-align: center;
-    text-transform: uppercase;
-    color: $secondary;
+  list-style-type: none;
+  float: left;
+  font-size: 14px;
+  text-align: center;
+  text-transform: uppercase;
+  color: $secondary;
 }
 
 .circle-container:before {
-      width: 40px;
-      height: 40px;
-      content: '';
-      line-height: 38px;
-      border: 2px solid $secondary;
-      display: block;
-      text-align: center;
-      margin: 0 auto 10px auto;
-      border-radius: 50%;
-      background-color: $light;
-  }
+    width: 40px;
+    height: 40px;
+    content: '';
+    line-height: 38px;
+    border: 2px solid $secondary;
+    display: block;
+    text-align: center;
+    margin: 0 auto 10px auto;
+    border-radius: 50%;
+    background-color: $light;
+}
 
-  .circle-container:first-child:after {
-      content: none;
-  }
-  .circle-container.complete {
-      color: $success;
-  }
-  .circle-container.complete:before {
-      border-color: $success;
-      background-color: $success;
-  }
-  .circle-container.running {
-      color: $primary;
-  }
-  .circle-container.running:before {
-      border-color: $primary;
-      background-color: $primary;
-  }
-  .circle-container.error {
-      color: $danger;
-  }
-  .circle-container.error:before {
-      border-color: $danger;
-      background-color: $danger;
-  }
-  .circle-container.error:after {
-      border-color: $danger;
-  }
+.circle-container:first-child:after {
+    content: none;
+}
+.circle-container.warning {
+    color: $warning
+}
 
-  .circle-container li.complete + li:after {
-      background-color: $success;
-  }
+.circle-container.warning::before {
+  border-color: $warning;
+  background-color: $warning;
+}
+.circle-container.complete {
+    color: $success;
+}
+.circle-container.complete:before {
+    border-color: $success;
+    background-color: $success;
+}
+.circle-container.running {
+    color: $primary;
+}
+.circle-container.running:before {
+    border-color: $primary;
+    background-color: $primary;
+}
+.circle-container.error {
+    color: $danger;
+}
+.circle-container.error:before {
+    border-color: $danger;
+    background-color: $danger;
+}
+.circle-container.error:after {
+    border-color: $danger;
+}
 
-  .centre {
-      position: middle;
-  }
-  .progress-text {
-      vertical-align: middle;
-  }
+.circle-container li.complete + li:after {
+    background-color: $success;
+}
+
+.centre {
+    position: middle;
+}
+.progress-text {
+    vertical-align: middle;
+}
 </style>
